7cde9129af6d3d3aa0a4b8ed74d66e1745beb43d
liudonghua123 <liudonghua123@gmail.com>
2017-10-20 17:20:39 +0800

    initial commit

---
+++ server/datasources.production.js
@@ -0,0 +1,9 @@
+module.exports = {
+  imp: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_DATABASE || 'imp',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+  }
+};
